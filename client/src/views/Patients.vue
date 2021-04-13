8cae27da8dd910d44a4f49e690199c1dabce5e3d
Abdourahamane <abdourahamane.issakasani@usmba.ac.ma>
2021-04-13 14:23:58 +0000

    database

---
+++ client/src/views/Patients.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="list row">
+        <div class="col-md-6">
+            <h4>Patients List</h4>
+            <ul>
+                <li v-for="(patient, index) in patients" :key="index">
+                    {{ patient.nom }}
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-6"></div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return { patients: [] };
+    },
+    methods: {
+        /* eslint-disable no-console */
+        getPatients() {
+            axios.get('http://localhost:8080/api/patients')
+                .then(response => {
+                    this.patients = response.data; // JSON are parsed automatically.
+                    console.log(response.data);
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        },
+    },
+    mounted() {
+        this.getPatients();
+    },
+};
+</script>
+
+<style scoped>
+.list {
+  text-align: left;
+  max-width: 450px;
+  margin: auto;
+}
+</style>
--- src/event-bus.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-const eventBus = new Vue({
-    methods: {},
-});
-
-export default eventBus;
